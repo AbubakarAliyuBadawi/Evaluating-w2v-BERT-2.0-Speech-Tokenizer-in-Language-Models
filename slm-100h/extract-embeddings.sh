fab9d137df040ec6c64f923afe74d5bb83e67e55
Abubakar Aliyu Badawi <151796014+AbubakarAliyuBadawi@users.noreply.github.com>
2024-04-27 22:36:23 +0200

    Create extract-embeddings.sh

---
+++ slm-100h/extract-embeddings.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH --job-name=slm
+#SBATCH --output=slm_emb_%j.out
+#SBATCH --error=slm_emb_%j.err
+#SBATCH --time=30:00:00 
+#SBATCH --partition=mundus
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32G
+#SBATCH --cpus-per-task=8
+#SBATCH --nodelist=mundus-mir-2
+
+# Load module necessary for the initial environment
+module load python/3.8
+module load cuda/11.0
+
+# Activate the Conda environment
+source /mundus/abadawi696/miniconda3/etc/profile.d/conda.sh
+conda activate slm
+
+# Increase NumExpr max threads, if applicable
+export NUMEXPR_MAX_THREADS=16
+
+# Change directory to your project directory
+cd /mundus/abadawi696/slm_project
+
+# Run your Python script
+/mundus/abadawi696/miniconda3/envs/slm/bin/python extract_embeddings.py
+
+# Deactivate Conda environment
+conda deactivate
